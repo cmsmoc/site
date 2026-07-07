14614563c05270056b34a5b0303e899771d83ac9
Alexandre Dourado <oalexandredourado@gmail.com>
2026-07-07 00:41:21 -0300

    Refactor SSVP Hub into a mobile-first PWA, upgrade convite background, add new markdown assistant and intro tutorial

---
+++ ssvp/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ssvp-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  '/ssvp/imgs/logotipo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return from cache
+        }
+        return fetch(event.request);
+      })
+  );
+});
